c391d0aff8f2094ea284eaf0295fff9c736963a9
Luís Zimn <154926679+luiszimn@users.noreply.github.com>
2024-01-13 01:07:59 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+let heroi = "Zimn"
+let heroiXP = 9800
+let nivelHeroi = " "
+
+
+if(heroiXP < 1000){
+    nivelHeroi = "Ferro"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)
+} 
+if((heroiXP >= 1001) && (heroiXP <= 2000)){
+    nivelHeroi = "Bronze"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)
+}
+if((heroiXP >= 2001) && (heroiXP <= 5000)){
+    nivelHeroi = "Prata"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)
+}
+if((heroiXP >= 5001) && (heroiXP <= 7000)){
+    nivelHeroi = "Ouro"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)    
+}
+if((heroiXP >= 7001) && (heroiXP <= 8000)){
+    nivelHeroi = "Platina"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)    
+}
+if((heroiXP >= 8001) && (heroiXP <= 9000)){
+    nivelHeroi = "Ascendente"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)    
+}
+if((heroiXP >= 9001) && (heroiXP <= 10000)){
+    nivelHeroi = "Imortal"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)    
+}
+if(heroiXP >= 10001){
+    nivelHeroi = "Radiante"
+    console.log("O Herói de nome " + heroi + " está no nivel de : " + nivelHeroi)    
+}
